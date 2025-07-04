b047bc0902bd9925a740f94b9ba9933015253a1e
DenVitaKaninen <tobiaskjernell@gmail.com>
2025-07-04 17:06:42 +0200

    added thunks into project, example tests and implemented in async return function

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,13 +1,14 @@
 const initialStateAccount = {
     balance: 0,
     loan: 0,
-    loanPurpose: ''
+    loanPurpose: '',
+    isLoading: false
 };
 
 export default function accountReducer(state = initialStateAccount, action) {
     switch (action.type) {
         case 'account/deposit':
-            return { ...state, balance: state.balance + action.payload };
+            return { ...state, balance: state.balance + action.payload, isLoading: false };
         case 'account/withdraw':
             return { ...state, balance: state.balance - action.payload };
         case 'account/requestLoan':
@@ -15,14 +16,27 @@ export default function accountReducer(state = initialStateAccount, action) {
             return { ...state, loan: action.payload.amount, loanPurpose: action.payload.purpose, balance: state.balance + action.payload.amount };
         case 'account/payLoan':
             return { ...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan }
+        case 'account/convertingCurrency':
+            return { ...state, isLoading: true }
         default:
             return state;
 
     }
 }
 
-export const deposit = (amount) => {
-    return { type: 'account/deposit', payload: amount }
+export const deposit = (amount, currency) => {
+    if (currency === 'USD')
+        return { type: 'account/deposit', payload: amount }
+
+    return async function (dispatch, getState) {
+        dispatch({ type: 'account/convertingCurrency' });
+        //API CALL
+        const res = await fetch(`https://api.frankfurter.dev/v1/latest?base=USD&symbols=${currency}`);
+        const data = await res.json();
+
+        const convertedAmount = (amount * data.rates[currency].toFixed(2))
+        dispatch({ type: 'account/deposit', payload: convertedAmount });
+    }
 }
 export const withdraw = (amount) => {
     return { type: 'account/withdraw', payload: amount }
